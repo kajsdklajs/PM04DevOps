7db18e90f80bb5f53ae2f04a520d3c0601160000
kajsdklajs <sfgsfgsfsdg@gmail.com>
2025-12-02 14:09:08 +0300

    Добавлена практика 2

---
+++ RandomPointsGraph/Form1.Designer.cs
@@ -0,0 +1,31 @@
+﻿namespace RandomPointsGraph
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Name = "MainForm";
+            this.Text = "График случайных точек";
+            this.ResumeLayout(false);
+        }
+    }
+}
